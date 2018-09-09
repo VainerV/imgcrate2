5dcc3be2c21dd51f373e5e4475aadda2141fe5c7
Vadim Vainer <vainerxv@gmail.com>
2018-09-08 18:13:39 -0700

    added Post request through form AJAX

--- public/src/controller.js
+++ public/src/controller.js
@@ -5,17 +5,18 @@ function enableListeners() {
   }
 
   function signUp() {
-      let newUser = {}
+      
     // submit new user Sign up information
     $(".signupbtn").on("click", event => {
         event.preventDefault();
-      let newUser = {};
+
+     
         let name = {
             userName: $("#firstName").val(),
             lastName: $("#lastName").val(),
         }
       
-     newUser = {
+   let  newUser = {
         user: name,
         userName: $('#userName').val(),
         password: $('#password').val(),
@@ -23,11 +24,26 @@ function enableListeners() {
 
      }
      
-     $.post( "/users/signup", JSON.stringify(newUser))
-     .done(function( data ) {
-       alert( "Data Loaded: " + data );
-     });
-      console.log(newUser);
+//console.log(newUser)
+      $.ajax({
+        type: 'POST',
+        url: "http://localhost:8080/users/signup",
+        data: newUser, //JSON.stringify(newUser),
+        dataType: "json",
+        success: function(data) { alert("Save Complete"),  console.log(data)},
+        error: function(error) {
+            console.log("New user wasnt created");
+          }
+  });
+  
+
+
+    //  //console.log(JSON.stringify(newUser));
+    //  $.post( "/users/signup", JSON.stringify(newUser))
+    //  .done(function( data ) {
+    //    alert( "Data Loaded: " + data );
+    //  });
+      //console.log(newUser);
    });
 
      
