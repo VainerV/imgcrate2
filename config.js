5cd6b8042d126bb7b97122bc8ef191fd67b03247
Vadim Vainer <vainerxv@gmail.com>
2018-09-11 10:17:26 -0700

    added molter s3, file-uplad.js

--- config.js
+++ config.js
@@ -7,6 +7,12 @@ exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/v_img_cr
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/v_img_crate_test';
 exports.PORT = process.env.PORT || 8080;
 
+//AWS
+exports.AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY
+exports.AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID
+exports.AWS_REGION = process.env.AWS_REGION
+
+
 //Authentication
 exports.JWT_SECRET = process.env.JWT_SECRET || 'secret';
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
